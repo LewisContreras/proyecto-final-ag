9cbe1350bb8640bd729b7ed32afe4c8085de081b
LewisContreras <legiconba4@gmail.com>
2021-05-29 21:25:52 -0500

    Coloco el provider de chakra

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { store } from './store/store';
 import { Provider } from 'react-redux'
 import { AppRouter } from './routers/AppRouter';
-
+import { ChakraProvider } from "@chakra-ui/react";
+ 
 export const App = () => {
     return (
-        <Provider store={store}>
-            <AppRouter />
-        </Provider>
+        <ChakraProvider>
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>
+        </ChakraProvider>
+        
     )
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,16 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Routes from "../routers/Routes";
-import { Provider } from 'react-redux'
+// import { ChakraProvider } from "@chakra-ui/react";
+// import Routes from "../routers/Routes";
+// import { Provider } from 'react-redux'
 
 
 
-function App() {
-  return (
-    <ChakraProvider>
-      <Routes/>
+// function App() {
+//   return (
+//     <ChakraProvider>
+//       <Routes/>
       
-    </ChakraProvider>
-  );
-}
+//     </ChakraProvider>
+//   );
+// } 
 
-export default App;
+// export default App;
