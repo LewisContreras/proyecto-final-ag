a7bf55053144eb326aed30332efc1ae10a4effd1
LewisContreras <legiconba4@gmail.com>
2021-06-14 20:46:25 -0500

    Arreglo orden de cards

--- src/components/MainPage/CardsContainer.jsx
+++ src/components/MainPage/CardsContainer.jsx
@@ -2,15 +2,16 @@ import { Flex, Link } from '@chakra-ui/layout'
 import React from 'react'
 import EachPetCard from './EachPetCard.jsx'
 import { Link as ReachLink } from 'react-router-dom';
+import { Grid } from '@chakra-ui/react';
 
 function CardsContainer() {
     return (
-        <Flex justifyContent="space-between" wrap="wrap" mt="100px" width="90%" mx="auto">
+        <Grid templateColumns="repeat(auto-fill,160px)" gap="20px" justifyContent="space-evenly" mt="100px" width="90%" mx="auto" pb="20px"  className="scene_element scene_element--fadeinup" >
             {
                 Array(8).fill("").map((el, index) => <Link key={index} as={ReachLink} to={`/detail/${index}`} ><EachPetCard id={index}  /></Link> )
             }
             
-        </Flex>
+        </Grid>
     )
 }
 
